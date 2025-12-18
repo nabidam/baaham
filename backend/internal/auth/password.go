13a35010c85ce5f8be4c8651259214255579eb84
nabidam <navidabbaspoor@gmail.com>
2025-12-18 22:48:18 +0330

    password helper updated

--- backend/internal/auth/password.go
+++ backend/internal/auth/password.go
@@ -5,3 +5,8 @@ import "golang.org/x/crypto/bcrypt"
 func ComparePassword(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
