ac41f9aee8eca91dd3a18719bcb6a0e26f0f06be
nabidam <navidabbaspoor@gmail.com>
2025-12-20 19:11:41 +0330

    add swaggo config into api

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -6,6 +6,11 @@ import (
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	docs "github.com/nabidam/baaham/cmd/docs"
+
 	"github.com/nabidam/baaham/internal/config"
 	"github.com/nabidam/baaham/internal/handler"
 	"github.com/nabidam/baaham/internal/route"
@@ -16,6 +21,7 @@ func New(
 	h *handler.MainHandler,
 ) *gin.Engine {
 	r := gin.New()
+	docs.SwaggerInfo.BasePath = "/api/v1"
 
 	// log all requests
 	r.Use(ginzap.Ginzap(cfg.Logger, time.RFC3339, true))
@@ -23,6 +29,9 @@ func New(
 	r.Use(ginzap.RecoveryWithZap(cfg.Logger, true))
 
 	route.RegisterRoutes(r, h)
+
+	// swagger route
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
 }
